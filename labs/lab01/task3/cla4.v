e544962ce85cfa32e4119baa75419e4d3afc6544
ABEER TULLI <f20240372@pilani.bits-pilani.ac.in>
2026-08-29 12:09:49 +0000

    lab01 task3

--- labs/lab01/task3/cla4.v
+++ labs/lab01/task3/cla4.v
@@ -29,54 +29,49 @@ module cla4(
 
   wire p0, p1, p2, p3;
   wire g0, g1, g2, g3;
-  wire c1, c2, c3, c4;
+  wire c1, c2, c3;
 
-  // TODO: your gate-level P/G, carry, and sum logic goes here.
-  and #(2) (g0, a[0], b[0]);
-  and #(2) (g1, a[1], b[1]);
-  and #(2) (g2, a[2], b[2]);
-  and #(2) (g3, a[3], b[3]);
 
   xor #(2) (p0, a[0], b[0]);
   xor #(2) (p1, a[1], b[1]);
   xor #(2) (p2, a[2], b[2]);
   xor #(2) (p3, a[3], b[3]);
 
-wire t1_0;
-wire t2_0, t2_1;
-wire t3_0, t3_1, t3_2;
-wire t4_0, t4_1, t4_2, t4_3;
-
-and #(2) (t1_0, p0, cin);
-
-and #(2) (t2_0, p1, g0);
-and #(2) (t2_1, p1, p0, cin);
-
-and #(2) (t3_0, p2, g1);
-and #(2) (t3_1, p2, p1, g0);
-and #(2) (t3_2, p2, p1, p0, cin);
-
-and #(2) (t4_0, p3, g2);
-and #(2) (t4_1, p3, p2, g1);
-and #(2) (t4_2, p3, p2, p1, g0);
-and #(2) (t4_3, p3, p2, p1, p0, cin);
-
-or #(2) (c1, g0, t1_0);
-or #(2) (c2, g1, t2_0, t2_1);
-or #(2) (c3, g2, t3_0, t3_1, t3_2);
-or #(2) (c4, g3, t4_0, t4_1, t4_2, t4_3);
-
-assign cout=c4;
-
-xor #2 (sum[0], p0, cin);
-xor #2 (sum[1], p1, c1);
-xor #2 (sum[2], p2, c2);
-xor #2 (sum[3], p3, c3);
+  and #(2) (g0, a[0], b[0]);
+  and #(2) (g1, a[1], b[1]);
+  and #(2) (g2, a[2], b[2]);
+  and #(2) (g3, a[3], b[3]);
 
 
+  wire c1_t1;
+  and #(2) (c1_t1, p0, cin);
+  or  #(2) (c1, g0, c1_t1);
 
 
+  wire c2_t1, c2_t2;
+  and #(2) (c2_t1, p1, g0);
+  and #(2) (c2_t2, p1, p0, cin);
+  or  #(2) (c2, g1, c2_t1, c2_t2);
 
-  // (cout should be connected to c4.) Remember the delay on every gate.
+  
+  wire c3_t1, c3_t2, c3_t3;
+  and #(2) (c3_t1, p2, g1);
+  and #(2) (c3_t2, p2, p1, g0);
+  and #(2) (c3_t3, p2, p1, p0, cin);
+  or  #(2) (c3, g2, c3_t1, c3_t2, c3_t3);
+
+  
+  wire c4_t1, c4_t2, c4_t3, c4_t4;
+  and #(2) (c4_t1, p3, g2);
+  and #(2) (c4_t2, p3, p2, g1);
+  and #(2) (c4_t3, p3, p2, p1, g0);
+  and #(2) (c4_t4, p3, p2, p1, p0, cin);
+  or  #(2) (cout, g3, c4_t1, c4_t2, c4_t3, c4_t4);
+
+ 
+  xor #(2) (sum[0], p0, cin);
+  xor #(2) (sum[1], p1, c1);
+  xor #(2) (sum[2], p2, c2);
+  xor #(2) (sum[3], p3, c3);
 
 endmodule
